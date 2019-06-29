e8bf3ff49ee739d58f5612e6acc2d573245f4c6b
foxbuka <krushenitskii@gmail.com>
2019-06-29 15:43:17 +0300

    register and auth

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const testCont = require('../controllers/testController');
+
 const landingControlMoks = require('../controllers/landingContMoks');
 
 
-router.get('/', testCont.getTest);
-router.post('/register', testCont.postTest);
+
 
 
 //My(MAKSIM) Test Code
@@ -15,7 +14,7 @@ router.get('/logout',landingControlMoks.logout);
 router.get('/loginmoks',landingControlMoks.getLogin);
 router.post('/loginmoks',landingControlMoks.postLogin);
 
-router.get('/moks', landingControlMoks.getMoksLand);
+router.get('/', landingControlMoks.getMoksLand);
 router.post('/moks', landingControlMoks.postMoksland);
 
 router.get('/moks/:name',landingControlMoks.getAvatar);
