3f5f8f3df7f2e485595c8f024785c07db188796e
maksim <skarobogyodko@gmail.com>
2019-06-30 02:20:28 +0300

    fix and rename router and controllers

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var gameControl = require('../controllers/gameController');
 
 router.get('/friendFish', gameControl.getFishing);
-router.post('/frend-fish',  gameControl.postFish);
+router.post('/friendFish',  gameControl.postFish);
 
 //test
 router.get('/friend/:link', gameControl.getFishPage);
