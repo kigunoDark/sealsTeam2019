8153e904f8af88b19d0358a5ae50297fe85fa561
foxbuka <krushenitskii@gmail.com>
2019-06-29 18:36:22 +0300

    mail

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,3 +1,14 @@
+const nodemailer = require('nodemailer');
+const sendgridTransport= require('nodemailer-sendgrid-transport');
+const env = require('dotenv').config();
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth:{
+        api_key: process.env.SENDGRID_API_KEY
+    }
+}));
+
+
 exports.getFishing = (req, res) => {
     res.render('./moks/fishing');
 }
@@ -5,7 +16,18 @@ exports.postFish = ( req, res) =>{
     var name = req.body.name;
     var surname = req.body.surname;
     var hobby = req.body.hobby;
-    console.log(name)
-    console.log(surname);
-    console.log(hobby);
+    var email = req.body.email;
+        
+            res.redirect('/');
+            return transporter.sendMail({
+                to: email,
+                from: 'kiguno1996@gmail.com',
+                subject: "Ты можешь стать кем угодно!!",
+                html: `<p style="text-align: justify; padding: 5%; margin: 0 auto;">Поздравляем ${name} ${surname}, у вас есть уникальная возможность!</p>
+                <p style="text-align: justify; padding: 5%; margin: 0 auto;">Станьте участником нашего конкурса и получите приз в размере 5000000 рублей, раелизовав мечту ${hobby}.</p>`,
+                
+
+            }); 
+         
+       
 }
--- data/database.js
+++ data/database.js
@@ -4,7 +4,5 @@ const sequelize = new Sequelize('testHak2019','root', '1995op1995',
 {
     dialect:'mysql',
     host:'localhost',
-  
 });
-
 module.exports = sequelize;
