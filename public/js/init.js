4627ca07517d3848f32861d457c69dcae83a028c
KSFirstUser <rusakk@mail.ru>
2019-06-30 09:13:52 +0300

    Added score

--- public/js/init.js
+++ public/js/init.js
@@ -2,5 +2,8 @@
   $(function() {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.main-page-nav__img').on('click', function() {
+      $(this).attr('data-after', 'bar');
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
